ba9120908879d8de8d6ad816faba616d56d515fc
W0490903 <146239352+W0490903@users.noreply.github.com>
2024-11-28 18:10:52 -0400

    Add files via upload

---
+++ data/projects/softwareprojects.js
@@ -0,0 +1,34 @@
+export default [
+	{
+		projectName: "Pizza Ordering ChatBot",
+		image: "images/pizzaorderchatbot.png",
+		summary:
+			"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+		preview: "https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+		techStack: ["Dailogflow", "Firebase"],
+	},
+	{
+		projectName: "WhatsApp-Bot",
+		image: "images/whatsappbot.jpg",
+		summary:
+			"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+		preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+		techStack: ["Selenium", "Chrome Webdriver", "Python"],
+	},
+	{
+		projectName: "Bill Generator",
+		image: "images/billgenerator.png",
+		summary:
+			"GUI to transfer data to excel sheets and generate bills on the local shops.",
+		preview: "https://github.com/vinaysomawat/Bill-Generator",
+		techStack: ["Tkinter", "Openxlpy", "Python"],
+	},
+	{
+		projectName: "Nodejs-Web-Crawling",
+		image: "images/nodejswebcrawling.png",
+		summary:
+			"Developed an application to send HTTP requests to a particular URL and then by extracting HTML of that web page for getting useful information.",
+		preview: "https://github.com/vinaysomawat/Nodejs-Web-Crawling",
+		techStack: ["HTML", "Nodejs", "JavaScript"],
+	}
+];
